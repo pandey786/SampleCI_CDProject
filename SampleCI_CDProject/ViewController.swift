2929c3d4065e77e1ad418ee14274ea485e493b7a
Durgesh Pandey <>
2018-12-04 13:24:26 +0530

    Test Commit 5

--- SampleCI_CDProject/ViewController.swift
+++ SampleCI_CDProject/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
-        print("Test Comment 4")
+        print("Test Comment 5")
     }
     
     override func didReceiveMemoryWarning() {
